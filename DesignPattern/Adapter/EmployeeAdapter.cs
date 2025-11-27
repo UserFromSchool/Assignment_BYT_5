f88465bea21193000718638607e146132142056d
jakub <jakub.puacz@gmail.com>
2025-11-27 19:32:52 +0100

    Implemented tasks.

--- DesignPattern/Adapter/EmployeeAdapter.cs
+++ DesignPattern/Adapter/EmployeeAdapter.cs
@@ -3,11 +3,22 @@ namespace DesignPattern.Adapter
 {
     public class EmployeeAdapter : ITarget
     {
-        private readonly BillingSystem thirdPartyBillingSystem = new();
+        private readonly BillingSystem billingSystem = new();
 
-        //public void ProcessCompanySalary(...)
-        //{
-        //    ...
-        //}
+        public void ProcessCompanySalary(string[,] employees)
+        {
+            List<Employee> employeesList = new List<Employee>();
+
+            for (int i = 0; i < employees.GetLength(0); i++)
+            {
+                int id = int.Parse(employees[i, 0]);
+                string name = employees[i, 1];
+                string designation = employees[i, 2];
+                decimal salary = decimal.Parse(employees[i, 3]);
+
+                employeesList.Add(new Employee(id, name, designation, salary));
+            }
+            billingSystem.ProcessSalary(employeesList);
+        }
     }
 }
